d55db415333f450343d099b13135357496b8628b
ABENEZER-SELESHI <ebenezerseleshi@gmail.com>
2026-04-07 23:40:31 +0300

    add cors permission

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const express = require("express");
 const cors = require("cors");
 
 // Allow all origins (quick fix)
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 const app = express();
 
